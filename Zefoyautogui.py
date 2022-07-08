74bd1e54f87a383233709854bf99a5669855d47f
EXCET <107319949+EXCET@users.noreply.github.com>
2022-07-08 04:13:47 -0400

    Update Zefoyautogui.py

--- Zefoyautogui.py
+++ Zefoyautogui.py
@@ -10,7 +10,7 @@ count = 0
 while (count < 999999):
   pyautogui.click()
   pyautogui.click(x=986, y=273)
-  pyautogui.write("https://www.tiktok.com/@exc3t/video/7106225446177623302?is_from_webapp=1&sender_device=pc&web_id=7047354921838970373")
+  pyautogui.write("EnterVideoLinkHere")
   pyautogui.press('enter')
   pyautogui.click(x=870, y=320)
   time.sleep(1)
